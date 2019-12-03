bd1fa8c3bec5a449d0c81f352bca9f8c98b2c792
gdosulli <45126172+gdosulli@users.noreply.github.com>
2019-12-03 12:04:00 -0500

    Added bgThread worker to update image from server while user has the response activity open

--- GERALD_app/app/src/main/java/com/example/gerald_app/ResponseActivity.java
+++ GERALD_app/app/src/main/java/com/example/gerald_app/ResponseActivity.java
@@ -25,6 +25,8 @@ public class ResponseActivity extends AppCompatActivity {
 
     private Button btnReset, btnPolice;
     private ImageView imgCapture;
+    private static boolean imageCheck;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +34,29 @@ public class ResponseActivity extends AppCompatActivity {
         setContentView(R.layout.activity_response);
 
         final RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        // set imageCheck to true while user is on response page
+        imageCheck = true;
+
+        // Get image from server and populate image on UI with it
+        String url = "http://10.245.66.214:5000/getimg";
+        int maxWidth = imgCapture.getMaxWidth();
+        int maxHeight = imgCapture.getMaxHeight();
+        final ImageRequest imageRequest = new ImageRequest(url, new Response.Listener<Bitmap>() {
+            @Override
+            public void onResponse(Bitmap response) {
+                // Assign the response to imgCapture
+                imgCapture.setImageBitmap(response);
+            }
+        }, maxWidth, maxHeight, ImageView.ScaleType.CENTER_INSIDE, null, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error)
+            {
+                error.printStackTrace();
+            }
+        });
+        //add request to queue
+        requestQueue.add(imageRequest);
 
         // create instances of buttons in activity
         btnReset = findViewById(R.id.btnReset);
@@ -49,13 +74,35 @@ public class ResponseActivity extends AppCompatActivity {
                 }
         });
 
-        // take and get image from server
-        // ImageRequest code from: https://medium.com/@manuaravindpta/networking-using-volley-library-39c22061b4ba
-        /*btnGetImg.setOnClickListener(new View.OnClickListener() {
+
+        // reset button brings user back to main activity page with the option to initialize the security system again
+        btnReset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //POST method
-                String url = "http://10.245.66.214:5000/getimg";
+                // exit bgThread
+                imageCheck = false;
+
+                // bring user back to main activity
+                openMainActivity();
+            }
+        });
+
+
+        // start bgThread to update image from server
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                updateImg();
+            }
+        }).start();
+    }
+
+    // method to keep updating image from server while user is on response page
+    private void updateImg() {
+        final RequestQueue requestQueue = Volley.newRequestQueue(this);
+        while (imageCheck) {
+            try {
+                String url = "http://169.254.91.218:5000/getimg";
                 int maxWidth = imgCapture.getMaxWidth();
                 int maxHeight = imgCapture.getMaxHeight();
                 ImageRequest imageRequest = new ImageRequest(url, new Response.Listener<Bitmap>() {
@@ -73,19 +120,21 @@ public class ResponseActivity extends AppCompatActivity {
                 });
                 //add request to queue
                 requestQueue.add(imageRequest);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        });*/
 
-
-        // reset button brings user back to main activity page with the option to initialize the security system again
-        btnReset.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // bring user back to main activity
-                openMainActivity();
+            // wait 4 seconds before getting image again
+            try {
+                Thread.sleep(4000);
             }
-        });
+            catch(InterruptedException ex)
+            {
+                Thread.currentThread().interrupt();
+            }
+        }
     }
+
 
     // opens main activity
     private void openMainActivity() {
